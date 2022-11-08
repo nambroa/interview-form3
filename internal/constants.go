caa238c5d8b5c483fc643feb2ff6fe5e7297fb70
Nicolas <ambroanicolas@gmail.com>
2022-11-08 16:50:03 -0300

    Remove comment.

--- internal/constants.go
+++ internal/constants.go
@@ -1,7 +1,5 @@
 package internal
 
-// Improvements: make the dockerfile hostname for the fake api not hardcoded.
-
 const BaseURL string = "http://fake-api:8080"
 const V1API string = "/v1"
 const AccountPrefix string = "/organisation/accounts"
